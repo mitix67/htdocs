bade26ea5b26fd527095e2e1a130218f0d285350
Mateusz Burzynski <mateuszburzynki@gmail.com>
2024-03-06 00:05:24 +0100

    partially working calendar

--- eventListeners.js
+++ eventListeners.js
@@ -1,3 +1,5 @@
+
+if (document.getElementById('brand') != null) {
 document.getElementById('brand').addEventListener('change', function() {
     var brand = this.value;
     var model = document.getElementById('model');
@@ -14,3 +16,63 @@ document.getElementById('brand').addEventListener('change', function() {
 document.getElementById('sendMessageButton').addEventListener('click', function() {
     document.getElementById('playground').innerHTML = '';
 });
+
+}
+let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+function convertDateNameToNumber(dateName) {
+    const monthIndex = months.findIndex(month => month.toLowerCase() === dateName.toLowerCase());
+    return monthIndex !== -1 ? monthIndex + 1 : null;
+}
+
+if(document.getElementById('calendar-btn-left') != null) {
+    document.getElementById('calendar-btn-left').addEventListener('click', function() {
+        var date = document.getElementById('month-year');
+        date = date.innerHTML.split(' ');
+        var month = date[0];
+
+        var currentDate = new Date();
+        month = convertDateNameToNumber(month) - 1;
+        console.log(month);
+        currentDate.setMonth(month);
+        var year = currentDate.getFullYear();
+        var day = currentDate.getDate();
+        var formattedDate = year + '-' + month + '-' + day;
+        // Send the formattedDate to index.html or perform any other desired action
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'index.php', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                var doc = document.querySelector('html');
+                doc.innerHTML = xhr.responseText;
+            }
+        }
+        xhr.send('formattedDate=' + formattedDate);
+    });
+
+    document.getElementById('calendar-btn-right').addEventListener('click', function() {
+        var date = document.getElementById('month-year');
+        date = date.innerHTML.split(' ');
+        var month = date[0];
+
+        var currentDate = new Date();
+        month = convertDateNameToNumber(month) + 1;
+        console.log(month);
+        currentDate.setMonth(month);
+        var year = currentDate.getFullYear();
+        var day = currentDate.getDate();
+        var formattedDate = year + '-' + month + '-' + day;
+        // Send the formattedDate to index.html or perform any other desired action
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'index.php', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                var doc = document.querySelector('html');
+                doc.innerHTML = xhr.responseText;
+            }
+        }
+        xhr.send('formattedDate=' + formattedDate);
+    });
+}
--- index.php
+++ index.php
@@ -12,6 +12,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
     <!-- Core theme CSS (includes Bootstrap)-->
     <link href="styl.css?v=<?php echo time() ?>" rel="stylesheet" />
+    <link href="calendar.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
@@ -82,6 +83,27 @@
                 </div>
             </div>
         </section>
+        <section>
+        <?php require_once 'calendar.php';
+
+
+$calendar = new Calendar(); 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Check if the "formattedDate" field is set in the POST request
+  if (isset($_POST["formattedDate"])) {
+      // Get the value from the "formattedDate" field
+      $date = $_POST["formattedDate"];
+      $calendar = new Calendar($date);
+      // Now you can use the $date variable in your script
+  } else {
+      echo "No date received.";
+  }}
+echo $calendar;
+?>
+        </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="eventListeners.js"></script>
+
   </body>
 </html>
